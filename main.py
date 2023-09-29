2ab69a405f8e426641c6b74abc2735cc124e4484
rohannpa <rohannp@uw.edu>
2023-09-29 13:53:16 -0700

    bot outline

--- main.py
+++ main.py
@@ -1,20 +1,27 @@
 import logging
 from telegram.ext import *
+import mysql.connector
 import responses
 
+mydb = mysql.connector.connect(host="locaclhost", user="root", host="password123")
 API_KEY = '6037321190:AAHGwdvHhJi78wQAz5ALTHmN5jEWZspAuGg'
 
 # set up the logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 logging.info('Starting Bot...')
 
-def start_command(update, context):
-    update.message.reply_text('Hi I am a Bot')
+def signup_command(update, context):
+    update.message.reply_text('Enter your first name')
+    update.message.reply_text('Enter your last name')
+    update.message.reply_text('Enter your phone number')
+    update.message.reply_text('Enter the department you do seva in')
+    update.message.reply_text('Are you the lead of this department? (YES/NO)')
 
-def help_command(update, context):
+
+def budgetrequest_command(update, context):
         update.message.reply_text('Hi I am here to help')
 
-def custom_command(update, context):
+def receiptsubmission_command(update, context):
             update.message.reply_text('This is a custom command')
 
 def handle_message(update, context):
@@ -33,9 +40,9 @@ if __name__ == '__main__':
     dp = updater.dispatcher
 
     # Commands
-    dp.add_handler(CommandHandler('start', start_command))
-    dp.add_handler(CommandHandler('help', help_command))
-    dp.add_handler(CommandHandler('custom', custom_command))
+    dp.add_handler(CommandHandler('signup', signup_command))
+    dp.add_handler(CommandHandler('budgetrequest',  budgetrequest_command))
+    dp.add_handler(CommandHandler('receiptsubmission', receiptsubmission_command))
 
     # Messages
     dp.add_handler(MessageHandler(Filters.text, handle_message))
@@ -46,6 +53,3 @@ if __name__ == '__main__':
     # Run the Bot
     updater.start_polling(2.0)
     updater.idle()
-
-
-
